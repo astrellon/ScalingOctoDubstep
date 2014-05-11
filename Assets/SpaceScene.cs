5dea7f28edae09af358a1d048ac392db611a4077
Astrellon <sovereign250@gmail.com>
2014-05-11 17:03:02 +1000

    - Space scene manager

---
+++ Assets/SpaceScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject prefab = Resources.LoadAssetAtPath<GameObject>("Assets/Meshes/Ship1/hull.obj");
+		GameObject obj = GameObject.Instantiate (prefab) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
