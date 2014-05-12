1c74914cae877f9991eeda9ef9f54a1014a22905
Astrellon <sovereign250@gmail.com>
2014-05-12 19:06:16 +1000

    - Updates to test mesh

--- Assets/Scripts/Space/SpaceScene.cs
+++ Assets/Scripts/Space/SpaceScene.cs
@@ -5,7 +5,7 @@ public class SpaceScene : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GameObject prefab = Resources.LoadAssetAtPath<GameObject>("Assets/Meshes/Ship1/hull.obj");
+		GameObject prefab = Resources.LoadAssetAtPath<GameObject>("Assets/Meshes/Ship1/Ship1.prefab");
 		GameObject obj = GameObject.Instantiate (prefab) as GameObject;
 	}
 	
